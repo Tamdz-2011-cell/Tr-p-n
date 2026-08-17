23325abfb5fde19712154a0327fd752c87c5bf9a
Tamdz-2011-cell <kadoya731@gmail.com>
2026-08-17 11:08:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ HTML_LAYOUT = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TÂM DZ TRA CỨU WEB</title>
+    <title>TamdzXAI WEB</title>
     <style>
         * {
             box-sizing: border-box;
